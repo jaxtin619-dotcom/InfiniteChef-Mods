28828ac796461ad03146851a7456fa86aad11d39
jaxtin619-dotcom <jaxtin619@gmail.com>
2025-12-19 16:39:04 -0600

    Create Bubba.js

---
+++ Bubba.js
@@ -0,0 +1,45 @@
+addIngredient("Mickey" ,{
+  type:"meat",
+  color:"000000",
+  shape:"circle_ears",
+});
+
+addTool("bloody poo"), {
+  func: function(placed) {placed.h = 0; },
+  shape:"swirl_pile"
+})
+
+addIngredient("happy_emoji"), {
+  type:"meat",
+  color:"ffff00",
+  shape:"face_smile",
+});
+
+addIngredient("sad_emoji"), {
+  type:"meat",
+  color:"ffff00",
+  shape:"face_frown",
+});
+
+addIngredient("meh_emoji"), {
+  type:"meat",
+  color:"ffff00",
+  shape:"face",
+});
+
+addIngredient("red_amongus"), {
+  type:"meat",
+  color:"fc0303",
+  shape:"sus",
+});
+
+addIngredient("blue_amongus"), {
+  type:"meat",
+  color:"035efc",
+  shape:"sus",
+});
+
+addIngredient("yellow_amongus"), {
+  type:"meat",
+  color:"ffff00",
+  shape:"sus",
